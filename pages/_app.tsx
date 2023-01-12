30ff918b9e6cdd0400885e72e183a89a694f7b6c
hamzafaham111 <hamzafaham111@gmail.com>
2023-01-12 15:46:16 +0500

    hello

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,11 +9,15 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Quick Repair</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        {/* <link
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap"
           rel="stylesheet"
-        />
+        /> */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
       <Navbar />
       <Component {...pageProps} />
